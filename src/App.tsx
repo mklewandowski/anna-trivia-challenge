b4e8b25c4fe9aa2a9573ce8f675b7fb83f486259
mklewandowski <matt.kain.lewandowski@gmail.com>
2022-12-15 09:19:05 -0800

    Add initial start screen layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState}  from 'react';
+import anna from "./anna.png";
 import './App.css';
 
 function App() {
+  const [started, setStarted] = useState(false);
+  const [complete, setComplete] = useState(false);
+
+  const onStartClick = () => {
+    setStarted(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-shell">
+        <header className="App-header">
+          <img src={anna} width={"150px"} />
+          Quiz Time With Anna
+          <div className="app-header-sub">a time waster by therealmattkain</div>
+        </header>
+        {started
+          ? <div>started</div>
+          : <>
+              <div className="intro-text">Ok, hot shot. It's you from the past. Let's take a little quiz and see how well you remember our childhood. You'll need to dig deep into the dusty memories of Super 18 (Milwaukee's Superstation), the Sears Christmas Catalog, and all things plastic. Or maybe it will be super easy. Are you ready?</div>
+              <button className="start-button" onClick={onStartClick}>START</button>
+            </>
+        }
+      </div>
     </div>
   );
 }
